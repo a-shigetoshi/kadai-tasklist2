159cc653c67f40d340d6a46948c592981e290a0b
a-shigetoshi <a-shigetoshi@i-enter.co.jp>
2024-05-16 01:36:07 +0000

    1

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;    
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // public function index()
+    // {
+    //      $tasks = Task::all(); 
+         
+    //      return view('tasks.index', [     
+    //         'tasks' => $tasks,        
+    //     ]);   
+    // }
+    public function index()
+    {
+        // ユーザーがログインしているかどうかをチェック
+        if (\Auth::check()) {
+            // ログインしている場合は、ログインユーザーのタスクを取得
+            $user = \Auth::user();
+            $tasks = Task::orderBy('created_at', 'desc')->paginate(10);
+    return view('tasks.index', compact('tasks'));
+        } else {
+            // ログインしていない場合は、ログインページにリダイレクト
+            return redirect()->route('login');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $task = new Task;
+         
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+   public function store(Request $request)
+    {
+        $request->validate([
+            'status' => 'required|max:10',   
+            'content' => 'required|max:255',
+        ]);
+    
+        $user = auth()->user(); // 現在のログインユーザーを取得
+        $task = new Task;
+        $task->content = $request->content;
+        $task->status = $request->status ?? 'default';
+        $task->user_id = $user->id; // ユーザーのIDを設定
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+        
+        if ($task->user_id !== \Auth::id()) {
+            return redirect('/'); // トップページにリダイレクト
+        }
+
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $task = Task::findOrFail($id);
+        
+        if ($task->user_id !== \Auth::id()) {
+        return redirect('/');
+    }
+
+       
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        if ($task->user_id !== \Auth::id()) {
+            return redirect('/');
+        }
+        
+        $request->validate([
+            'status' => 'required|max:10',   
+            'content' => 'required|max:255',
+        ]);
+    
+       
+    
+        $task->status = $request->status;  
+        $task->content = $request->content;
+        $task->save();
+    
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+       $task = Task::findOrFail($id);
+
+        if (\Auth::id() === $task->user_id) {
+            $task->delete();
+            return redirect('/')
+                ->with('success', 'Delete Successful');
+        }
+
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+//use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController; //追記
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [TasksController::class, 'index']);
+
+Route::get('/dashboard', [TasksController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::resource('microposts', TasksController::class, ['only' => ['store', 'destroy']]);
+ // Task related routes
+   Route::resource('tasks', TasksController::class);
+
+    
+});
+
+
+require __DIR__.'/auth.php';
